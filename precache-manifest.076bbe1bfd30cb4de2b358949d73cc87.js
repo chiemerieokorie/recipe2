9d012dc1f8fae7bc3ab8abd3b94219a5c85bbd1e
Chief <emerieokorie@gmail.com>
2020-02-09 11:52:01 +0100

    Updates

---
+++ precache-manifest.076bbe1bfd30cb4de2b358949d73cc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11ff88c23581b8d95f22681ea19b437",
+    "url": "/recipe2/index.html"
+  },
+  {
+    "revision": "479f4596acaf37e600a7",
+    "url": "/recipe2/static/css/main.4102d5d2.chunk.css"
+  },
+  {
+    "revision": "01bb9eabb2a7f9e1304c",
+    "url": "/recipe2/static/js/2.cc893c62.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/recipe2/static/js/2.cc893c62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "479f4596acaf37e600a7",
+    "url": "/recipe2/static/js/main.d938af4b.chunk.js"
+  },
+  {
+    "revision": "f439254267e686c2a4a7",
+    "url": "/recipe2/static/js/runtime-main.7467030e.js"
+  }
+]);
